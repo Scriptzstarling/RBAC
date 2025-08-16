2dba347915fc584737ec8951f7b373a366aefa1f
Scriptzstarling <shashwat11muz@gmail.com>
2025-08-16 13:01:42 +0530

    protected routes

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const verifyToken = require("../middleware/authMiddleware");
+const router = express.Router();
+
+//Only admin can access this router
+router.get("/admin", verifyToken, (req, res) => {
+    res.json({ message:"Welcome Admin" });
+});
+//Both admin and manager can access this router
+router.get("/manager", verifyToken, (req, res) => {
+    res.json({ message:"Welcome Manager" });
+});
+
+//All can access this router
+router.get("/user", verifyToken, (req, res) => {
+    res.json({ message:"Welcome User" });
+});
+
+module.exports = router;
